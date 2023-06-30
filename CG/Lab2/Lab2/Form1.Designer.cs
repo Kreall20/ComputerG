8be63985e598f39bd44bdd2ba5ef6a6718ec5889
Kirill <ivzenkokirill@gmail.com>
2023-06-30 14:45:01 +0600

    first

---
+++ CG/Lab2/Lab2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(23, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(433, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 55);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Find Treasure";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Location = new System.Drawing.Point(847, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 55);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Save New Image";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(654, 132);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(276, 263);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1040, 487);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1058, 534);
+            this.MinimumSize = new System.Drawing.Size(1058, 534);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ CG/Lab2/Lab2/Geometriccharacteristics.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public static class Geometriccharacteristics
+    {
+        public static int GetArea(int[,] labels, int label)
+        {
+            int area = 0;
+            for (int y = 0; y < labels.GetLength(1); y++)
+            {
+                for (int x = 0; x < labels.GetLength(0); x++)
+                {
+                    if (labels[x, y] == label)
+                    {
+                        area++;
+                    }
+                }
+            }
+            return area;
+        }
+        public static Tuple<int, int> GetCentroid(int[,] labels, int label)
+        {
+            int area = GetArea(labels, label);
+            int cx = 0;
+            int cy = 0;
+            for (int y = 0; y < labels.GetLength(1); y++)
+            {
+                for (int x = 0; x < labels.GetLength(0); x++)
+                {
+                    if (labels[x, y] == label)
+                    {
+                        cx += x;
+                        cy += y;
+                    }
+                }
+            }
+            cx /= area;
+            cy /= area;
+            return Tuple.Create(cx, cy);
+        }
+
+        public static int GetPerimeter(int[,] labels, int label)
+        {
+            int perimeter = 0;
+            for (int y = 0; y < labels.GetLength(1); y++)
+            {
+                for (int x = 0; x < labels.GetLength(0); x++)
+                {
+                    if (labels[x, y] == label)
+                    {
+                        if (x == 0 || x == labels.GetLength(1) - 1 || y == 0 || y == labels.GetLength(0) - 1)
+                        {
+                            perimeter++;
+                        }
+                        else if (labels[x,y-1] != label || labels[x,y+1] != label || labels[x - 1,y] != label || labels[x+1,y] != label)
+                        {
+                            perimeter++;
+                        }
+                    }
+                }
+            }
+            return perimeter;
+        }
+        public static double GetCompactness(int[,] labels, int label)
+        {
+            int area = GetArea(labels, label);
+            int perimeter = GetPerimeter(labels, label);
+            double compactness = 4 * Math.PI * area / (perimeter * perimeter);
+            return compactness;
+        }
+        public static double GetOrientation(int[,] labels, int label)
+        {
+            var center = GetCentroid(labels, label);
+            double cx = center.Item1;
+            double cy = center.Item2;
+            double ixx = 0;
+            double iyy = 0;
+            double ixy = 0;
+            for (int y = 0; y < labels.GetLength(1); y++)
+            {
+                for (int x = 0; x < labels.GetLength(0); x++)
+                {
+                    if (labels[x, y] == label)
+                    {
+                        double dx = x - cx;
+                        double dy = y - cy;
+                        ixx += dx * dx;
+                        iyy += dy * dy;
+                        ixy += dx * dy;
+                    }
+                }
+            }
+            return 0.5 * Math.Atan2(2 * ixy, ixx - iyy);
+        }
+        public static double GetEccentricity(int[,] labels, int label)
+        {
+            int area = GetArea(labels, label);
+            var center = GetCentroid(labels, label);
+            double cx = center.Item1;
+            double cy = center.Item2;
+            double ixx = 0;
+            double iyy = 0;
+            double ixy = 0;
+            for (int y = 0; y < labels.GetLength(1); y++)
+            {
+                for (int x = 0; x < labels.GetLength(0); x++)
+                {
+                    if (labels[x, y] == label)
+                    {
+                        double dx = x - cx;
+                        double dy = y - cy;
+                        ixx += dx * dx;
+                        iyy += dy * dy;
+                        ixy += dx * dy;
+                    }
+                }
+            }
+            double lambda1 = (ixx + iyy + Math.Sqrt((ixx - iyy) * (ixx - iyy) + 4 * ixy * ixy)) / (2 * area);
+            double lambda2 = (ixx + iyy - Math.Sqrt((ixx - iyy) * (ixx - iyy) + 4 * ixy * ixy)) / (2 * area);
+            double eccentricity = Math.Sqrt(1 - Math.Min(lambda1, lambda2) / Math.Max(lambda1, lambda2));
+            return eccentricity;
+        }
+    }
+}
